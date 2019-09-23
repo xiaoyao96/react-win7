6f0333e49f5f20c5b601a3adc3fdb289690edd55
yx675258207 <675258207@qq.com>
2019-09-23 18:22:40 +0800

    fix: 改成http

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 
-export const baseUrl = process.env.NODE_ENV === 'production' ? 'https://api.xiaoyao.pro' : 'http://localhost:3000';
+export const baseUrl = process.env.NODE_ENV === 'production' ? 'http://api.xiaoyao.pro:3000' : 'http://localhost:3000';
 // export const baseUrl = 'http://39.108.181.148:3000';
 // export const baseUrl = 'http://localhost:3000';
 
